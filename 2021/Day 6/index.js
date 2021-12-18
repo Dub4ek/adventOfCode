f4b68a69dd31e2c6bfa01bc455d837164a2802f0
Andrei Starodubtcev <dub4ek@gmail.com>
2021-12-18 16:26:42 +0300

    complete day 6

--- 2021/Day 6/index.js
+++ 2021/Day 6/index.js
@@ -1,5 +1,22 @@
 function solution(data) {
+  const collection = data.split(',').map(item => parseInt(item, 10));
+  let countDays = 0;
 
+  while(countDays < 80) {
+    for (let i = 0; i < collection.length; i++) {
+      let item = collection[i];
+
+      if (item > 0) {
+        collection[i] -= 1;
+      } else if (item === 0){
+        collection[i] = 6;
+        collection.push(9);
+      }
+    }
+    countDays += 1;
+  }
+
+  return collection.length;
 }
 
 function solution2(data) {
@@ -16,3 +33,4 @@ fs.readFile('./input.txt', 'utf8' , (err, data) => {
   console.log(solution(data));
 })
 
+//console.log(solution('3,4,3,1,2'));
