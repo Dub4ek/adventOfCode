f63dce9ee5d6605612149011a01b212f8d2a7057
Andrei Starodubtcev <dubchek@bk.ru>
2021-12-27 20:55:33 +0300

    start day 6

---
+++ 2021/Day 12/index.js
@@ -0,0 +1,22 @@
+function solution(data) {
+
+}
+
+const fs = require('fs')
+
+
+/*fs.readFile('./input.txt', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(solution(data));
+})*/
+
+
+console.log(solution('11111\n' +
+  '19991\n' +
+  '19191\n' +
+  '19991\n' +
+  '11111'))
+
